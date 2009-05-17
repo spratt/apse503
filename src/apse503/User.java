3ff6d9e3f31bdd14c901c1c28334bc03e6e9770b
Simon Pratt <simon.pratt@edu.sait.ca>
2009-05-16 20:05:32 -0600

    Refactored User domain class; Added a unit test for each attribute validation.

--- src/apse503/User.java
+++ src/apse503/User.java
@@ -3,8 +3,6 @@ package apse503;
 public class User {
 	
 	public String userName,
-				  passwordHash,
-				  salt,
 				  firstName,
 				  lastName,
 				  email,
@@ -13,6 +11,9 @@ public class User {
 				  postalCode,
 				  province,
 				  country;
+	
+	private String passwordHash,
+				   salt;
 	
 	public User() {
 		this.userName = "";
@@ -27,6 +28,22 @@ public class User {
 		this.province = "";
 		this.country = "";
 	}
+	
+	// Create a new user from an old user
+	// Mostly for testing purposes, but could serve other uses ;-)
+	public User(User originalUser) {
+		this.userName = originalUser.userName;
+		this.passwordHash = originalUser.passwordHash;
+		this.salt = originalUser.salt;
+		this.firstName = originalUser.firstName;
+		this.lastName = originalUser.lastName;
+		this.email = originalUser.email;
+		this.address = originalUser.address;
+		this.city = originalUser.city;
+		this.postalCode = originalUser.postalCode;
+		this.province =originalUser.province;
+		this.country = originalUser.country;
+	}
 
 	public boolean isValid() {
 		// Check the attribute lengths
@@ -38,30 +55,45 @@ public class User {
 		validLength = validLength && email.length() >= 11;
 		validLength = validLength && address.length() >= 6;
 		validLength = validLength && city.length() >= 3;
+		// This is 5 to take US zip codes into consideration
 		validLength = validLength && postalCode.length() >= 5;
 		validLength = validLength && province.length() >= 6;
 		validLength = validLength && country.length() >= 6;
 		
-		// Check attribute uniqueness - implement later
+		// TODO Check attribute uniqueness
 		// username
 		// email
 		
 		return validLength;
 	}
 	
-	// Just a fake for now
-	public boolean save() {
-		if (this.isValid()) {
-			// Save to database
-			return true;
-		}
-		return false;
+	public boolean authenticate(String password) {	
+		return this.isValid() && this.encrypt(password) == this.passwordHash;
 	}
 	
-	// Not yet implemented
-	public static String encrypt(String toHash,String salt) {
+	public void setPassword(String password) {
+		this.passwordHash = this.encrypt(password);
+	}
+	
+	// TODO implement persistence code
+	public boolean save() {
+		if (!this.isValid()) return false;
+		// Save to database here
+		return true;
+	}
+
+	// TODO implement randomized salt generation
+	private void generateSalt() {
+		// Generate salt here
+		this.salt = "----5---10---15---20---25---30---35---40---45---50---55---60--64";
+	}
+	
+	// TODO implement proper encryption
+	private String encrypt(String toHash) {
 		// Good resource for when we need to implement this:
 		// http://www.owasp.org/index.php/Hashing_Java
+		this.generateSalt();
+		// Do some hashing here ;-)
 		return toHash;
 	}
 }
--- src/apse503/UserTest.java
+++ src/apse503/UserTest.java
@@ -1,27 +1,106 @@
 package apse503;
 
+import org.junit.Before;
+
 import junit.framework.TestCase;
 
 public class UserTest extends TestCase {
+	User validUser = new User();
+	
+	@Before
+	public void setUp() throws Exception {
+		// Valid User
+		validUser.firstName = "Foo";
+		validUser.lastName = "Bar";
+		validUser.address = "123 4th Street";
+		validUser.city = "Calgary";
+		validUser.province = "Alberta";
+		validUser.country = "Canada";
+		validUser.email = "foo@bar.net";
+		validUser.postalCode = "H3Z2K6";
+		validUser.userName = "foo";
+		validUser.setPassword("foobar");
+	}
+	
 	public void testBlankUserIsInvalid() throws Exception {
-		User myUser = new User();
-		assertFalse("A blank user should be invalid!",myUser.isValid());
+		assertFalse("A blank user should be invalid!",new User().isValid());
 	}
 	
 	public void testValidationOnValidUser() throws Exception {
-		User myUser = new User();
-		myUser.address = "123 4th Street";
-		myUser.city = "Calgary";
-		myUser.country = "Canada";
-		myUser.email = "foo@bar.net";
-		myUser.firstName = "Foo";
-		myUser.lastName = "Bar";
-		myUser.passwordHash = "----5---10---15---20---25---30---35---40---45---50---55---60--64";
-		myUser.postalCode = "H3Z2K6";
-		myUser.province = "Alberta";
-		myUser.salt = "----5---10---15---20---25---30---35---40---45---50---55---60--64";
-		myUser.userName = "foo";
-		assertTrue(myUser.isValid());
+		assertTrue(validUser.isValid());
+	}
+	
+	public void testShortFirstNameIsInvalid() {
+		User invalidUser = new User(validUser);
+		invalidUser.firstName = "-2";
+		assertFalse("A short first name should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testShortLastNameIsInvalid() {
+		User invalidUser = new User(validUser);
+		invalidUser.lastName = "-2";
+		assertFalse("A short last name should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testShortAddressIsInvalid() {
+		User invalidUser = new User(validUser);
+		invalidUser.address = "----5";
+		assertFalse("A short address should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testShortCityIsInvalid() {
+		User invalidUser = new User(validUser);
+		invalidUser.city = "-2";
+		assertFalse("A short city name should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testShortProvinceIsInvalid() {
+		User invalidUser = new User(validUser);
+		invalidUser.province = "----5";
+		assertFalse("A short province name should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testShortCountryIsInvalid() {
+		User invalidUser = new User(validUser);
+		invalidUser.country = "----5";
+		assertFalse("A short country name should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testShortEmailIsInvalid() {
+		User invalidUser = new User(validUser);
+		invalidUser.email = "----5---10";
+		assertFalse("A short email address should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testShortPostalCodeIsInvalid() {
+		User invalidUser = new User(validUser);
+		// Remember that the minimum is 5, due to US postal codes
+		invalidUser.postalCode = "---4";
+		assertFalse("A short postal code should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testShortUserNameIsInvalid() {
+		User invalidUser = new User(validUser);
+		invalidUser.userName = "-2";
+		assertFalse("A short user name should be invalid!",
+				invalidUser.isValid());
+	}
+	
+	public void testInvalidAuthenticationFails() {
+		assertFalse(validUser.authenticate("wrongpassword"));
+		assertFalse(new User().authenticate("wronguser"));
+	}
+	
+	public void testValidAuthenticationPasses() {
+		assertTrue(validUser.authenticate("foobar"));
 	}
 	
 	/* save this for later
