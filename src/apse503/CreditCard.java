ad06dabd4dc67a366cb232224fa6f0b401c043f8
Sunmoon Kim <sunmoon.kim@edu.sait.ca>
2009-06-08 18:43:29 +0100

    Create a credit card class to validate user.

---
+++ src/apse503/CreditCard.java
@@ -0,0 +1,95 @@
+package apse503;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CreditCard extends PersistenceClass {
+
+	public String card_type,
+				  card_number,
+				  card_holder_name;
+	
+	
+	public int card_id,
+			   card_expiry_month,
+			   card_expiry_year,
+			   card_code;
+	
+	
+	public CreditCard() {
+		this.card_type = "";
+		this.card_number = "";
+		this.card_holder_name = "";
+		this.card_id = -1;
+		this.card_expiry_month = -1;
+		this.card_expiry_year = -1;
+		this.card_code = -1;
+	
+	}
+	
+	public boolean isValid()
+	{
+		if(null == sql){ 
+			setUpDataSource();
+		}
+		try {
+			sql.execute("SELECT * FROM credit_cards WHERE card_type = "+
+					this.card_type +
+					" and card_number = "+this.card_number+
+					" and card_holder_name = "+this.card_holder_name+
+					" and card_expiry_month = "+this.card_expiry_month+
+					" and card_expiry_year = "+this.card_expiry_year+
+					" and card_code = "+this.card_code);
+			ResultSet results = sql.getResultSet();
+			if(!results.next()) 
+			
+				return false;
+			
+			else {
+				return true;
+			}
+		}catch (SQLException e) {
+			// TODO log exception
+			e.printStackTrace();
+		}
+	
+		return false;
+	}
+	
+	
+	// TODO implement persistence code
+	public boolean save() {
+		
+		return false;
+	}
+	
+	public ArrayList<CreditCard> getAll() {
+		if(null == sql) setUpDataSource();
+		try {
+			sql.execute("SELECT * FROM credit_cards");
+			ResultSet results = sql.getResultSet();
+
+			ArrayList<CreditCard> cc = new ArrayList<CreditCard>();
+			CreditCard tmp;
+			while(results.next())
+			{
+				tmp = new CreditCard();				
+				tmp.id = results.getInt("card_id");
+				tmp.card_type = results.getString("card_type");
+				tmp.card_number = results.getString("card_number");
+				tmp.card_holder_name = results.getString("card_holder_name");
+				tmp.card_expiry_month = results.getInt("card_expiry_month");
+				tmp.card_expiry_year = results.getInt("card_expiry_year");
+				tmp.card_code = results.getInt("card_code");
+				cc.add(tmp);
+			}
+			System.out.println("it works");
+			return cc;
+		} catch (SQLException e) {
+			// TODO log exception
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
